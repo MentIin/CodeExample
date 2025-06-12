5e3dd4df48704a1dce7ed8e9a7f422d375257c52
MentIin <81743613+MentIin@users.noreply.github.com>
2025-06-12 15:32:54 +0300

    fix(LoadProgressState): address potential problem with analytics service

--- CodeBase/Infrastructure/States/LoadProgressState.cs
+++ CodeBase/Infrastructure/States/LoadProgressState.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using Assets.CodeBase.Infrastructure;
@@ -10,31 +9,30 @@ using Assets.CodeBase.Infrastructure.Services.SaveLoad;
 using Assets.CodeBase.Infrastructure.Services.StaticData;
 using Assets.SimpleLocalization.Scripts;
 using CodeBase.Infrastructure.Data;
-using CodeBase.Infrastructure.Services.Analitics;
-using CodeBase.Infrastructure.Services.Analytics;
+using CodeBase.Infrastructure.Services.Analytics; 
 using UnityEngine;
 
 namespace CodeBase.Infrastructure.States
 {
     public class LoadProgressState : IState
     {
-        private GameStateMachine _stateMachine;
-        private IPersistentProgressService _progressService;
-        private ISaveLoadService _saveLoadService;
+        private readonly GameStateMachine _stateMachine;
+        private readonly IPersistentProgressService _progressService;
+        private readonly ISaveLoadService _saveLoadService;
         private readonly IStaticDataService _staticDataService;
         private readonly IUserInfoService _userInfoService;
         private readonly IAnalyticsService _analyticsService;
         private readonly PauseService _pauseService;
         private readonly ICoroutineRunner _coroutineRunner;
 
-
-        private Coroutine _analyticServiceCoroutine;
-        private bool _analyticServiceTimeout=false;
+        private Coroutine _analyticServiceTimeoutCoroutine;
+        private bool _hasAnalyticsInitialized = false;
+        private bool _isAnalyticsTimeoutReached = false;
 
         public LoadProgressState(GameStateMachine stateMachine,
             IPersistentProgressService progressService,
             ISaveLoadService saveLoadService, IStaticDataService staticDataService,
-             IUserInfoService userInfoService, IAnalyticsService analyticsService,
+            IUserInfoService userInfoService, IAnalyticsService analyticsService,
             PauseService pauseService, ICoroutineRunner coroutineRunner)
         {
             _stateMachine = stateMachine;
@@ -51,58 +49,62 @@ namespace CodeBase.Infrastructure.States
         {
             _pauseService.Pause();
             _staticDataService.Load();
-            if (_saveLoadService.LoadProgress() == null) _progressService.Progress = GetNewProgress();
-            else _progressService.Progress = _saveLoadService.LoadProgress();
 
+            _progressService.Progress = _saveLoadService.LoadProgress() ?? GetNewProgress();
 
             FixIncorrectSaves();
             ObtainStartModules();
-            
-            // require progress
+
             _analyticsService.Initialize();
-            _analyticsService.Initialized += EnterTwo;
-            _analyticServiceCoroutine = _coroutineRunner.StartCoroutine(AnalyticCoroutine());
+            _analyticsService.Initialized += OnAnalyticsInitialized;
+            _analyticServiceTimeoutCoroutine = _coroutineRunner.StartCoroutine(AnalyticTimeoutCoroutine());
         }
 
-        private IEnumerator AnalyticCoroutine()
+        public void Exit()
+        {
+            _analyticsService.Initialized -= OnAnalyticsInitialized; 
+            if (_analyticServiceTimeoutCoroutine != null)
+            {
+                _coroutineRunner.StopCoroutine(_analyticServiceTimeoutCoroutine);
+                _analyticServiceTimeoutCoroutine = null;
+            }
+        }
+
+        private IEnumerator AnalyticTimeoutCoroutine()
         {
             yield return new WaitForSecondsRealtime(3f);
-            if (_analyticServiceTimeout)
+
+            if (!_hasAnalyticsInitialized) 
             {
-                
-            }
-            else
-            {
-                EnterTwo();
+                _isAnalyticsTimeoutReached = true;
+                Debug.LogWarning("Analytics initialization timed out. Proceeding without full analytics setup.");
+                ProceedToNextState();
             }
         }
 
-        private void EnterTwo()
+        private void OnAnalyticsInitialized()
         {
-            if (_analyticServiceTimeout) return;
-            _analyticServiceTimeout = true;
-            
-            
-            List<KeyValuePair<int, MachineModel>> toDelete = new List<KeyValuePair<int, MachineModel>>();
-            foreach (var valuePair in _progressService.Progress.ShopMachineModels)
+            _analyticsService.Initialized -= OnAnalyticsInitialized;
+
+            if (_isAnalyticsTimeoutReached)
             {
-                if (_staticDataService.ForStartingMachine(valuePair.Key) == null)
-                {
-                    toDelete.Add(valuePair);
-                }
-                else if (valuePair.Value.MachineModules.Count != _staticDataService.ForStartingMachine(valuePair.Key).Modules.Length)
-                {
-                    toDelete.Add(valuePair);
-                }
+                Debug.LogWarning("Analytics initialized AFTER timeout. Ignoring late initialization.");
+                return;
             }
 
-            foreach (var keyValuePair in toDelete)
+            _hasAnalyticsInitialized = true;
+            if (_analyticServiceTimeoutCoroutine != null)
             {
-                _progressService.Progress.ShopMachineModels.Remove(keyValuePair.Key);
+                _coroutineRunner.StopCoroutine(_analyticServiceTimeoutCoroutine);
+                _analyticServiceTimeoutCoroutine = null;
             }
-            
-            //_stateMachine.Enter<LoadLevelState, int>(_progressService.Progress.LevelStage);
 
+            ProceedToNextState();
+        }
+
+        private void ProceedToNextState()
+        {
+            FixShopMachineModels(); 
 
             InitializeLocalization();
 
@@ -127,9 +129,24 @@ namespace CodeBase.Infrastructure.States
 
         private void FixIncorrectSaves()
         {
-            foreach (var VARIABLE in _progressService.Progress.AvaliableModules)
-            { 
-                
+            // TODO
+        }
+
+        private void FixShopMachineModels()
+        {
+            List<KeyValuePair<int, MachineModel>> toDelete = new List<KeyValuePair<int, MachineModel>>();
+            foreach (var valuePair in _progressService.Progress.ShopMachineModels)
+            {
+                if (_staticDataService.ForStartingMachine(valuePair.Key) == null ||
+                    valuePair.Value.MachineModules.Count != _staticDataService.ForStartingMachine(valuePair.Key).Modules.Length)
+                {
+                    toDelete.Add(valuePair);
+                }
+            }
+
+            foreach (var keyValuePair in toDelete)
+            {
+                _progressService.Progress.ShopMachineModels.Remove(keyValuePair.Key);
             }
         }
 
@@ -142,28 +159,17 @@ namespace CodeBase.Infrastructure.States
                     if (_progressService.Progress.GetModuleLevel(data.Id) == 0)
                     {
                         _progressService.Progress.ObtainModule(data, 1, false);
-                        Debug.Log(data.Id);
+                        // Debug.Log($"Obtained starting module: {data.Id}"); 
                     }
                 }
             }
-        }
-
-        public void Exit()
-        {
-            
-        }
-
-        private void LoadProgressOrInitNew()
-        {
-            _progressService.Progress =  _saveLoadService.LoadProgress() ?? GetNewProgress();
         }
 
         private PlayerProgress GetNewProgress()
         {
             PlayerProgress playerProgress = new PlayerProgress(0);
             
-            playerProgress.StartingMachineId = 0;
-
+            playerProgress.StartingMachineId = 0; 
 
             foreach (var data in _staticDataService.GetAllModules())
             {
